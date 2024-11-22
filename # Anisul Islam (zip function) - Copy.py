c0598a812fc1d7bfbb3bab49766b966d24167b28
Imran Hossen <jihad.programmer@gmail.com>
2024-11-22 21:04:00 +0600

    Add files via upload

---
+++ # Anisul Islam (zip function) - Copy.py
@@ -0,0 +1,48 @@
+# Anisul Islam
+
+# zip function
+roll_number = ['1','2','3','4','5','6','7','8']
+
+name = ['Jihad','Fahim','Ifaz','Nadim','Jafor','Rahad','Ramos','Sazid']
+
+print(list(zip(roll_number , name )))
+
+# Recursion
+def fact (n):
+
+    if n == 1:
+        return 1
+    else:
+        return (n*fact(n-1))
+print(fact(4))
+
+
+
+
+
+def freefire(x,y):
+    return x*y
+
+print(freefire(4,7))
+
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
