0f9227c28065b4a443b2e628b4c369f5f34536c0
Imran Hossen <jihad.programmer@gmail.com>
2024-12-11 20:04:01 +0600

    Add files via upload

---
+++ #str methods.py.py
@@ -0,0 +1,54 @@
+name = "Imran"
+print(name[-3:-1]) # ra
+print(name.upper()) # Will convert all strings in variable to uppercase.
+
+print(name.lower()) # imran
+print(name.find('r')) # 2 
+print(name.replace('Imran','Jihad')) # Jihad
+
+p = "Helllo I am Jiahd"
+print(p.rstrip("Jihad")) #the rstrip() removes any trailing characters. 
+print(p.count('I'))
+j = 'hello this is omar'
+print(j.capitalize())
+blogHeading = "introduction tO jS"
+print(blogHeading.capitalize())
+print(j[6:10]) # string sliching
+
+
+str2 = "Silver Spoon"
+print(str2.split(" "))      #Splits the string at the whitespace " ". 
+
+print(str2.center(40," "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
